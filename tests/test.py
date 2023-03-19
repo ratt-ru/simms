02620aa174dc7810cfcc4aeb2303cc4428f9ad0c
Thomson, Alec (S&A, Kensington) <AlecThomson@users.noreply.github.com>
2023-03-19 15:55:09 +0800

    Dont need casa

--- tests/test.py
+++ tests/test.py
@@ -16,9 +16,6 @@ Install it, then re-run the test.py
 # check if simms is installed
 subprocess.check_call(["simms", "--help"])
 
-# check if casapy is installed
-subprocess.check_call(["casa", '--help','--log2term','--nologger', '--nogui'])
-
 # Finally see if we can run simms
 subprocess.check_call(['simms', '-T', 'kat-7', '-st', '8', '-dt', '10'])
 
